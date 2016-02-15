b5b88bef84cfe7262c9d45686c8534bbda53e387
HebertC <qi8013so@go.minneapolis.edu>
2016-02-15 15:20:41 -0600

    Last modification  by Hebert C Add a button to show All items Initial database items added Fixed bug in database (update of a quantity of the same item )

--- gui_main.py
+++ gui_main.py
@@ -16,27 +16,18 @@ class MenuGUI():
         self.root.title("GUI for store data")
         # set the variable
         self.window_height = 500
-
         self.sidebar_width = 200
-
-        self.main_area_width = 400
-
+        self.main_area_width = 700
         self.sidebar_bg_color = 'gray'
-
         self.mainarea_bg_color = 'white'
-
         # calling the sidebar method
         self._set_sidebar()
-
         # initialize the frames with empty everything
         self.mainarea_tshirt = tk.Frame(self.root)
-
         self.mainarea_posters = tk.Frame(self.root)
-
         self.mainarea_boots = tk.Frame(self.root)
-
-        self.mainarea_exit = tk.Frame(self.root)
-
+        self.mainarea_boots = tk.Frame(self.root)
+        self.mainarea_show_all_items = tk.Frame(self.root)
         # calling the frame to show initially
         self._only_tshirt_show()
 
@@ -59,30 +50,44 @@ class MenuGUI():
         tshirt.place(bordermode = OUTSIDE, height = 50, width = self.sidebar_width, relx=.03, rely=.013)
 
         # adding the Poster button on right side-bar
-        # relx and rely are the attribute which set the postsion
+        # relx and rely are the attribute which set the position
         posters = tk.Button(self.root, text ="Posters", command=self._only_posters_show)
 
         posters.pack()
 
-        posters.place(bordermode = OUTSIDE, height = 50, width = self.sidebar_width,relx=.03, rely=.11)
+        posters.place(bordermode = OUTSIDE, height = 50, width = self.sidebar_width,relx=.03, rely=.12)
 
         # adding the Boots button on right side-bar
-        # relx and rely are the attribute which set the postsion
+        # relx and rely are the attribute which set the position
 
         boots = tk.Button(self.root, text ="Boots", command=self._only_boot_show)
 
         boots.pack()
 
-        boots.place(bordermode = OUTSIDE, height = 50, width = self.sidebar_width,relx=.03, rely=.21)
+        boots.place(bordermode = OUTSIDE, height = 50, width = self.sidebar_width,relx=.03, rely=.225)
+
+        """
+        adding the menu in the sidebar which will show
+        the item stored in the database
+        on the click event this will call the _show_all_items function
+        please the function _show_all_items definition for more detail
+        """
+        show_items = tk.Button(self.root, text ="Show All Items", command=self._show_all_items)
+
+        show_items.pack()
+
+        show_items.place(bordermode = OUTSIDE, height = 50, width = self.sidebar_width,relx=.03, rely=.333)
+
 
         # adding the Exit button on Left side-bar
-        # relx and rely are the attribute which set the postsion
+        # relx and rely are the attribute which set the position
 
         exit = tk.Button(self.root, text ="Exit", command=quit)
 
         exit.pack()
 
         exit.place(bordermode = OUTSIDE, height = 50, width = self.sidebar_width,relx=.03, rely=.86)
+
 
     """
     This method will hide all the displays and show only the
@@ -96,10 +101,9 @@ class MenuGUI():
 
         # destroy the all frames
         self.mainarea_tshirt.destroy()
-
         self.mainarea_posters.destroy()
-
         self.mainarea_boots.destroy()
+        self.mainarea_show_all_items.destroy()
 
         self.root.update()
 
@@ -119,7 +123,8 @@ class MenuGUI():
         # adding the button on the text field
         # command attribute in button will execute the method or command
 
-        Entry(self.mainarea_tshirt, textvariable=self._storeVar, width=15).pack()
+        self.entry_mainarea_tshirt = Entry(self.mainarea_tshirt, textvariable=self._storeVar, width=15)
+        self.entry_mainarea_tshirt.pack()
 
         Button(self.mainarea_tshirt, text="Enter",width=15, command=self._enter_data).pack()
 
@@ -133,10 +138,9 @@ class MenuGUI():
 
         # destroy the all frames
         self.mainarea_tshirt.destroy()
-
         self.mainarea_posters.destroy()
-
         self.mainarea_boots.destroy()
+        self.mainarea_show_all_items.destroy()
 
 
         self.root.update()
@@ -157,7 +161,8 @@ class MenuGUI():
         # adding the button on the text field
         # command attribute in button will execute the method or command
 
-        Entry(self.mainarea_posters, textvariable=self._storeVar, width=15).pack()
+        self.entry_mainarea_posters = Entry(self.mainarea_posters, textvariable=self._storeVar, width=15)
+        self.entry_mainarea_posters.pack()
 
         Button(self.mainarea_posters, text="Enter",width=15, command=self._enter_data).pack()
 
@@ -174,6 +179,7 @@ class MenuGUI():
         self.mainarea_tshirt.destroy()
         self.mainarea_posters.destroy()
         self.mainarea_boots.destroy()
+        self.mainarea_show_all_items.destroy()
 
         self.root.update()
 
@@ -191,9 +197,53 @@ class MenuGUI():
         # adding the Entry (text-field) on frame
         # adding the button on the text field
         # command attribute in button will execute the method or command
-        Entry(self.mainarea_boots, textvariable=self._storeVar, width=15).pack()
-
+        self.entry_mainarea_boots = Entry(self.mainarea_boots, textvariable=self._storeVar, width=15)
+        self.entry_mainarea_boots.pack()
         Button(self.mainarea_boots, text="Enter",width=15, command=self._enter_data).pack()
+
+        self.root.mainloop()
+
+    """
+    This method will invoke when the
+    Show All Items button is clicked
+    This method will do these steps
+    - close all the frames and show only the
+    Show All Items frame
+    -
+    """
+
+    def _show_all_items(self):
+
+        # destroy all the frames
+        self.mainarea_tshirt.destroy()
+        self.mainarea_posters.destroy()
+        self.mainarea_boots.destroy()
+        self.mainarea_show_all_items.destroy()
+        self.root.update()
+
+        self.root.minsize(self.main_area_width,self.window_height)
+
+        self.mainarea_show_all_items = tk.Frame(self.root, bg = self.mainarea_bg_color, width = self.main_area_width, height = self.window_height)
+
+        self.mainarea_show_all_items.pack(expand = True, fill = 'both', side = 'right')
+
+        # adding the label with the text "All T-shirts"
+        tshirt_string = StringVar()
+        Label(self.mainarea_show_all_items, textvariable=tshirt_string, width=18).pack()
+        tshirt_string.set("All T-shirts\n ===========================")
+        self._get_all_data("tshirts")
+        # adding the label with the text "All Posters"
+        poster_string = StringVar()
+        Label(self.mainarea_show_all_items, textvariable=poster_string, width=18).pack()
+        poster_string.set("\n\nAll Poster\n ==============================")
+        self._get_all_data("posters")
+        # adding the label with the text "All Boots"
+        boots_string = StringVar()
+        Label(self.mainarea_show_all_items, textvariable=boots_string, width=18).pack()
+        boots_string.set("\n\n All Boots\n =============================")
+        self._get_all_data("boots")
+
+
 
         self.root.mainloop()
 
@@ -208,6 +258,15 @@ class MenuGUI():
         cursor = db.cursor()
         cursor.execute('insert into %s (name) values (?)'%self._tablename, (storeVar,))
         db.commit()
+        # Empty the ENTRY widget
+        if self._tablename == "tshirts":
+            self.entry_mainarea_tshirt.delete(0, 'end')
+        elif self._tablename == "boots":
+            self.entry_mainarea_boots.delete(0, 'end')
+        else:
+            self.entry_mainarea_posters.delete(0, 'end')
+
+
         """
         select data from the database and show them
         on the console.
@@ -215,6 +274,29 @@ class MenuGUI():
         for row in cursor.execute('SELECT * FROM %s'%self._tablename):
             print(row)
         db.close()
+
+    """
+    get the all data from database
+    """
+    def _get_all_data(self, tbl_name):
+        print (tbl_name)
+        db = sqlite3.connect("database_filename")
+        cursor = db.cursor()
+        """
+        select data from the database with the count and show them
+        on the label.
+        count and group by in this query calculate the distinct values
+        in the table
+        later in the loop these values assign to the Label
+        """
+        for row in cursor.execute('SELECT name, count(name) FROM %s group by name'%tbl_name):
+            all_item_string = StringVar()
+            Label(self.mainarea_show_all_items, textvariable=all_item_string, width=18,justify=CENTER).pack()
+            string= str(row[0]) +" ---- "+str(row[1])
+            all_item_string.set(string)
+            print (row)
+        db.close()
+
 
 
 
@@ -241,6 +323,8 @@ def setup_database():
     # Create a database table
     # add the id as integer primary so it will relate to the unique
     # identification
+    # this will add the name of the item
+    # and will update the entry of no of items
     cursor.execute('''CREATE TABLE tshirts
         (id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
         name TEXT NOT NULL
@@ -253,6 +337,23 @@ def setup_database():
         (id INTEGER PRIMARY KEY ASC AUTOINCREMENT,
         name TEXT NOT NULL
         );''');
+
+    """
+    adding the initial data
+    """
+    cursor.execute('''INSERT INTO tshirts(name) VALUES('Broncos');''');
+    cursor.execute('''INSERT INTO tshirts(name) VALUES('Chiefs');''');
+    cursor.execute('''INSERT INTO tshirts(name) VALUES('Riders');''');
+    cursor.execute('''INSERT INTO tshirts(name) VALUES('Cardinals');''');
+    cursor.execute('''INSERT INTO tshirts(name) VALUES('Vikings');''');
+    cursor.execute('''INSERT INTO tshirts(name) VALUES('Eagles');''');
+    cursor.execute('''INSERT INTO posters(name) VALUES('Bears');''');
+    cursor.execute('''INSERT INTO posters(name) VALUES('Steelers');''');
+    cursor.execute('''INSERT INTO posters(name) VALUES('Browns');''');
+    cursor.execute('''INSERT INTO posters(name) VALUES('Packers');''');
+    cursor.execute('''INSERT INTO boots(name) VALUES('Bengals');''');
+    cursor.execute('''INSERT INTO boots(name) VALUES('Panthers');''');
+
 
     db.commit()
     db.close()
